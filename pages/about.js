7496a31271116a6fc568b30fe7cd9dfe7265c056
Leroux Martin <tintin_151@hotmail.com>
2020-05-10 17:44:37 -0400

    Jusqu'à #5

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+import Layout from '../components/Layout'
+
+export default () => (
+    <Layout title="About">
+        <p> A javascript programmer</p>
+        <img src="/javascript_logo.png" alt="Javascript" height= "200px" />
+    </Layout>
+);
---
+++ pages/hireme.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout'
+
+const HireMe = () => (
+    <Layout>
+        <h1> Hire me</h1>
+        <p>You can hire me at 
+            <a href="mailto:expertcoder@gmail.com">expertcoder@gmail.com</a>
+        </p>
+    </Layout>
+);
+
+export default HireMe;
