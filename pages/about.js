0ad8ce5493e7cec783e2e9e8de446a8f1f5ce868
Leroux Martin <tintin_151@hotmail.com>
2020-05-17 17:44:49 -0400

    Use useEffect to fetch data

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,22 @@
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
+import { useEffect, useState } from "react";
 
-export default () => (
-    <Layout title="About">
-        <p> A javascript programmer</p>
-        <img src="/javascript_logo.png" alt="Javascript" height= "200px" />
-    </Layout>
-);
+export default function About() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/reedbarger')
+        .then(res => res.json())
+        .then(data => {
+            setData(data);
+        }) 
+    });
+
+    return (
+        <Layout title="About">
+            {JSON.stringify(data)}
+            <p> A javascript programmer</p>
+            <img src="/javascript_logo.png" alt="Javascript" height= "200px" />
+        </Layout>
+    )
+};
