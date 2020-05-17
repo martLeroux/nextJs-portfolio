b694b0bf2c04d834a365713f818e8aa87e4293ae
Leroux Martin <tintin_151@hotmail.com>
2020-05-17 18:17:50 -0400

    Utilise les infos de l'api de git

--- pages/about.js
+++ pages/about.js
@@ -2,16 +2,14 @@ import Layout from '../components/Layout';
 import fetch from 'isomorphic-unfetch'
 
 function About({ user }) {
-
     return (<Layout title="About">
-        {JSON.stringify(user)}
-        <p> A javascript programmer</p>
-        <img src="/javascript_logo.png" alt="Javascript" height= "200px" />
+        <p>{user.name}</p>
+        <img src={user.avatar_url} alt="Reed" height= "200px" />
     </Layout>)
 }
 
 export async function getStaticProps() {
-    const res = await fetch('https://api.github.com/repos/zeit/next.js')
+    const res = await fetch('https://api.github.com/users/reedbarger')
     const user = await res.json()
     return { 
         props: {
